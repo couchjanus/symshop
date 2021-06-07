856267a813143ef6c9b4a4cce965f631018d955c
couchjanus <couchjanus@gmail.com>
2021-06-07 17:09:25 +0300

    added php engine

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Hello Nginx</title>
+	<title>Hello Nginx+php</title>
 </head>
 <body>
-	<h1>Hello World</h1>
+	<?="<h1>Hello Php World</h1>"?>
 </body>
 </html>
